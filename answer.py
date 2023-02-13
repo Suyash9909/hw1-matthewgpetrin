23345e2d197bc1c04feb0481de239e71937d9318
matthewgpetrin <matthewgpetrin@gmail.com>
2023-02-13 15:01:46 -0500

    .

--- answer.py
+++ answer.py
@@ -64,7 +64,7 @@ def list_1D():
 
     # Inplace sort list l (use .sort() ).
 
-    l.sort()
+    l = l.sort()
 
     # Get the 4th to 10th item in sorted list l and assign them to a new list new_l.
 
@@ -78,7 +78,9 @@ def list_2D():
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
 
-    A = [[1, 4, 5], [6, 10, 11], [12, 17, 38]]
+    A = [[1, 4, 5], 
+         [6, 10, 11], 
+         [12, 17, 38]]
 
     # Collect the items in the last column of matrix A using list comprehension and assign it to a new variable last_column.
 
@@ -90,7 +92,7 @@ def list_2D():
 
     # Get the item at row 2 and column 1 of A.
 
-    b = [1][0]
+    b = A[1][0]
 
 
     return A,last_column, a, b
