8e21002be60d077063077f68fe7c793c925bec7e
matthewgpetrin <matthewgpetrin@gmail.com>
2023-02-13 15:03:57 -0500

    .

--- answer.py
+++ answer.py
@@ -64,7 +64,7 @@ def list_1D():
 
     # Inplace sort list l (use .sort() ).
 
-    l = l.sort()
+    l.sort()
 
     # Get the 4th to 10th item in sorted list l and assign them to a new list new_l.
 
